b70abfff348288f1afccf693eef49529bea8cfa4
Dileep <dileepbatti00@gmail.com>
2024-08-05 21:03:29 +0530

    second commit

--- src/main/java/com/Empoyee/entity/Employee.java
+++ src/main/java/com/Empoyee/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.Empoyee.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Employee {
 	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
